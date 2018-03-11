4ecd7c30cd0b43cd6c96be9de4dbfea3d6348c06
xchovanecv1 <33866464+xchovanecv1@users.noreply.github.com>
2018-03-11 09:48:40 +0100

    Initial

---
+++ z4/photos.js
@@ -0,0 +1,19 @@
+var p = `{
+  "photos": [
+      {
+        "src": "fotka1.jpg",
+        "title": "Názov fotky 1",
+        "description": "Dlhší popis obrázku 1"
+      },
+      {
+        "src": "fotka2.jpg",
+        "title": "Názov fotky 2",
+        "description": "Dlhší popis obrázku 2"
+      },
+      {
+        "src": "fotka3.jpg",
+        "title": "Názov fotky 3",
+        "description": "Dlhší popis obrázku 3"
+      }      
+    ]
+}`
